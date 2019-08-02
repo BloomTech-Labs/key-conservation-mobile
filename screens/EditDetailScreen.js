f709405e44355c3a1b63f85169b7884cc74f3497
coriagogo <pkcparis@gmail.com>
2019-08-02 14:44:21 -0400

    labels added to edit details screen, can use next to cycle through input fields
    Co-authored-by: Matt Klein <mklein246@gmail.com>

--- screens/EditDetailScreen.js
+++ screens/EditDetailScreen.js
@@ -16,24 +16,22 @@ import { postUser } from '../store/actions';
 class EditDetailScreen extends React.Component {
   state = {
     email: this.props.currentUserProfile.email,
-    website: this.props.currentUserProfile.org_link_url,
+    orgLinkUrl: this.props.currentUserProfile.org_link_url,
+    orgLinkText: this.props.currentUserProfile.org_link_text,
     facebook: this.props.currentUserProfile.facebook,
     instagram: this.props.currentUserProfile.instagram,
     twitter: this.props.currentUserProfile.twitter,
     aboutUs: this.props.currentUserProfile.about_us,
     speciesHabitats: this.props.currentUserProfile.species_and_habitats,
-    issues: this.props.currentUserProfile.issues
+    issues: this.props.currentUserProfile.issues,
+    // supportUs: this.props.currentUserProfile.support_us,
+    orgCta: this.props.currentUserProfile.org_cta
   };
 
   // handlePress = async () => {
   //   const { error } = this.props;
   //   const { sub, role, email } = this.props.currentUser;
-  //   let user = {
-  //     username: this.state.usernameInput,
-  //     sub: sub,
-  //     roles: role,
-  //     email: email
-  //   };
+  //   let details = state
   //   await this.props.postUser(user);
   //   this.props.navigation.navigate(error ? 'CreateAccount' : 'Conservationist');
   // };
@@ -48,86 +46,164 @@ class EditDetailScreen extends React.Component {
         <ScrollView>
           <View style={styles.sectionContainer}>
             <View style={styles.Card} />
-              <TextInput
-                returnKeyType='go'
-                placeholder='Email'
-                style={styles.inputContain}
-                onChangeText={text => this.setState({ email: text })}
-                value={this.state.usernameInput}
-                required
-              />        
-            
-              <TextInput
-                returnKeyType='go'
-                placeholder='Website'
-                style={styles.inputContain}
-                onChangeText={text => this.setState({ website: text })}
-                value={this.state.usernameInput}
-                required
-              />
-            
-              {/* <TextInput
-                returnKeyType='go'
-                placeholder='Donation Link'
-                style={styles.inputContain}
-                onChangeText={text => this.setState({ donationLink: text })}
-                value={this.state.usernameInput}
-                required
-              /> */}
+              <View style={styles.sections}>
+                <Text style={styles.sectionsText}>Email</Text>
+                <TextInput
+                  ref={(input) => { this.emailInput = input; }}
+                  returnKeyType='next'
+                  placeholder='Email'
+                  style={styles.inputContain}
+                  onChangeText={text => this.setState({ email: text })}
+                  onSubmitEditing={() => { this.orgLinkUrlInput.focus(); }}
+                  blurOnSubmit={false}
+                  value={this.state.email}
+                />    
+              </View>
 
-              <TextInput
-                returnKeyType='go'
-                placeholder='Facebook'
-                style={styles.inputContain}
-                onChangeText={text => this.setState({ facebook: text })}
-                value={this.state.usernameInput}
-                required
-              />
+              <View style={styles.sections}>
+                <Text style={styles.sectionsText}>Website</Text>
+                <TextInput
+                  ref={(input) => { this.orgLinkUrlInput = input; }}
+                  returnKeyType='next'
+                  placeholder='Website'
+                  style={styles.inputContain}
+                  onChangeText={text => this.setState({ orgLinkUrl: text })}
+                  onSubmitEditing={() => { this.orgLinkTextInput.focus(); }}
+                  blurOnSubmit={false}
+                  value={this.state.orgLinkUrl}
+                />
+              </View>
 
-              <TextInput
-                returnKeyType='go'
-                placeholder='Instagram'
-                style={styles.inputContain}
-                onChangeText={text => this.setState({ instagram: text })}
-                value={this.state.usernameInput}
-                required
-              />
+              <View style={styles.sections}>
+                <Text style={styles.sectionsText}>Website Text</Text>
+                <TextInput
+                  ref={(input) => { this.orgLinkTextInput = input; }}
+                  returnKeyType='next'
+                  placeholder='Website Text'
+                  style={styles.inputContain}
+                  onChangeText={text => this.setState({ orgLinkText: text })}
+                  onSubmitEditing={() => { this.orgCtaInput.focus(); }}
+                  blurOnSubmit={false}
+                  value={this.state.orgLinkText}
+                />
+              </View>
 
-              <TextInput
-                returnKeyType='go'
-                placeholder='Twitter'
-                style={styles.inputContain}
-                onChangeText={text => this.setState({ twitter: text })}
-                value={this.state.usernameInput}
-                required
-              />
+              <View style={styles.sections}>  
+                <Text style={styles.sectionsText}>Donation Link</Text>            
+                <TextInput
+                  ref={(input) => { this.orgCtaInput = input; }}
+                  returnKeyType='next'
+                  placeholder='Donation Link'
+                  style={styles.inputContain}
+                  onSubmitEditing={() => { this.facebookInput.focus(); }}
+                  blurOnSubmit={false}
+                  value={this.state.orgCta}
+                />
+              </View>
 
-              <TextInput
-                returnKeyType='go'
-                placeholder='About Us'
-                style={styles.inputContain}
-                onChangeText={text => this.setState({ aboutUs: text })}
-                value={this.state.usernameInput}
-                required
-              />
+              <View style={styles.sections}>
+                <Text style={styles.sectionsText}>Facebook</Text>
+                <TextInput
+                  ref={(input) => { this.facebookInput = input; }}
+                  returnKeyType='next'
+                  placeholder='Facebook'
+                  style={styles.inputContain}
+                  onChangeText={text => this.setState({ facebook: text })}
+                  onSubmitEditing={() => { this.instagramInput.focus(); }}
+                  blurOnSubmit={false}
+                  value={this.state.facebook}
+                />
+              </View>
 
-              <TextInput
-                returnKeyType='go'
-                placeholder='Species & Habitats'
-                style={styles.inputContain}
-                onChangeText={text => this.setState({ speciesHabitats: text })}
-                value={this.state.usernameInput}
-                required
-              />
+              <View style={styles.sections}>
+                <Text style={styles.sectionsText}>Instagram</Text>
+                <TextInput
+                  ref={(input) => { this.instagramInput = input; }}
+                  returnKeyType='next'
+                  placeholder='Instagram'
+                  style={styles.inputContain}
+                  onChangeText={text => this.setState({ instagram: text })}
+                  onSubmitEditing={() => { this.twitterInput.focus(); }}
+                  blurOnSubmit={false}
+                  value={this.state.instagram}
+                />
+              </View>
 
-              <TextInput
-                returnKeyType='go'
-                placeholder='Big Issues'
-                style={styles.inputContain}
-                onChangeText={text => this.setState({ issues: text })}
-                value={this.state.usernameInput}
-                required
-              />  
+              <View style={styles.sections}>
+                <Text style={styles.sectionsText}>Twitter</Text>
+                <TextInput
+                  ref={(input) => { this.twitterInput = input; }}
+                  returnKeyType='next'
+                  placeholder='Twitter'
+                  style={styles.inputContain}
+                  onChangeText={text => this.setState({ twitter: text })}
+                  onSubmitEditing={() => { this.aboutUsInput.focus(); }}
+                  blurOnSubmit={false}
+                  value={this.state.twitter}
+                />
+              </View>
+
+              <View style={styles.sections}>
+                <Text style={styles.sectionsText}>About Us</Text>
+                <TextInput
+                  ref={(input) => { this.aboutUsInput = input; }}
+                  returnKeyType='next'
+                  placeholder='About Us'
+                  style={styles.inputContain2}
+                  onChangeText={text => this.setState({ aboutUs: text })}
+                  onSubmitEditing={() => { this.speciesHabitatsInput.focus(); }}
+                  blurOnSubmit={false}
+                  multiline={true}
+                  value={this.state.aboutUs}
+                />
+              </View>
+
+              <View style={styles.sections}>
+                <Text style={styles.sectionsText}>Species & Habitats</Text>
+                <TextInput
+                  ref={(input) => { this.speciesHabitatsInput = input; }}
+                  returnKeyType='next'
+                  placeholder='Species & Habitats'
+                  style={styles.inputContain2}
+                  onChangeText={text => this.setState({ speciesHabitats: text })}
+                  onSubmitEditing={() => { this.issuesInput.focus(); }}
+                  blurOnSubmit={false}
+                  multiline={true}
+                  value={this.state.speciesHabitats}
+                />
+              </View>
+
+              <View style={styles.sections}>
+                <Text style={styles.sectionsText}>Big Issues</Text>
+                <TextInput
+                  ref={(input) => { this.issuesInput = input; }}
+                  returnKeyType='next'
+                  placeholder='Big Issues'
+                  style={styles.inputContain2}
+                  onChangeText={text => this.setState({ issues: text })}
+                  onSubmitEditing={() => { this.supportUsInput.focus(); }}
+                  blurOnSubmit={false}
+                  multiline={true}
+                  value={this.state.issues}
+                />
+              </View>
+
+              {/* <View style={styles.sections}>
+                <Text style={styles.sectionsText}></Text>              
+                <TextInput
+                  ref={(input) => { this.supportUsInput = input; }}
+                  returnKeyType='next'
+                  placeholder='Support Us'
+                  style={styles.inputContain}
+                  onChangeText={text => this.setState({ supportUs: text })}
+                  onSubmitEditing={() => { this.orgCtaInput.focus(); }}
+                  blurOnSubmit={false}
+                  value={this.state.supportUs}
+                />
+              </View> */}
+
+              
+
               {/* <View style={styles.whiteSpace} /> */}
                         
           </View>
@@ -160,10 +236,11 @@ const styles = StyleSheet.create({
   sectionContainer: {
     flexDirection: 'column',
     flexWrap: 'wrap',
-    margin: 15
+    marginLeft: 15, 
+    marginRight: 15,
   },
   Card: {
-    marginTop: 20,
+    marginTop: 10,
     backgroundColor: '#fff',
     width: '100%',
     padding: 25
@@ -175,7 +252,16 @@ const styles = StyleSheet.create({
     padding: 5,
     borderRadius: 5,
     fontSize: 20,
-    marginBottom: 45,
+    marginBottom: 25,
+  },
+  inputContain2: {
+    height: 140,
+    borderWidth: 2,
+    borderColor: '#C4C4C4',
+    padding: 5,
+    borderRadius: 5,
+    fontSize: 20,
+    marginBottom: 25,
   },
   touchableButton: {
     paddingTop: 25,
@@ -195,6 +281,16 @@ const styles = StyleSheet.create({
     textTransform: 'uppercase',
     fontWeight: 'bold',
     letterSpacing: 2
+  },
+  sections: {
+    // marginTop: 20,
+    backgroundColor: '#fff',
+    width: '100%',    
+  },
+  sectionsText: {
+    fontSize: 20, 
+    fontWeight: 'bold',
+    marginBottom: 5,
   },
   whiteSpace:{
     height: 100,
