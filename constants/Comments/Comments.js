1e8edc147e66bd1980a82d3600a370d494969dc6
Katie Rose <katiefelten@gmail.com>
2019-09-26 12:55:42 -0400

    :zap: updated padding

--- constants/Comments/Comments.js
+++ constants/Comments/Comments.js
@@ -1,26 +1,28 @@
 export default {
   commentWrapper: {
-    flexDirection: 'column'
+    flexDirection: "column"
   },
   commentView: {
-    flexDirection: 'row'
+    flexDirection: "row"
   },
   replyView: {
-    flexDirection: 'row',
+    flexDirection: "row",
     marginTop: 20
   },
   username: {
-    marginLeft: 3,
-    fontWeight: 'bold',
-    fontFamily: 'OpenSans-SemiBold'
+    marginLeft: 7,
+    paddingBottom: 5,
+    marginTop: 5,
+    fontWeight: "bold",
+    fontFamily: "OpenSans-SemiBold"
   },
   commentBody: {
-    marginLeft: 3
+    marginLeft: 7
   },
   commentText: {
-    flexDirection: 'column',
+    flexDirection: "column",
     width: 300,
-    fontFamily: 'OpenSans-SemiBold'
+    fontFamily: "OpenSans-SemiBold"
   },
   input: {
     minHeight: 48,
@@ -32,24 +34,24 @@ export default {
     marginLeft: 5,
     marginRight: 5,
     marginBottom: 20,
-    backgroundColor: '#f7f9fc'
+    backgroundColor: "#f7f9fc"
   },
   inputWrapper: {
-    flexDirection: 'row',
+    flexDirection: "row",
     height: 48,
     width: 280,
     marginLeft: 5,
     borderRadius: 5,
-    backgroundColor: '#f7f9fc'
+    backgroundColor: "#f7f9fc"
   },
   interaction: {
-    flexDirection: 'row',
+    flexDirection: "row",
     marginBottom: 15
   },
   timeText: {
-    color: '#929292',
+    color: "#929292",
     fontSize: 10,
-    marginLeft: 38,
+    marginLeft: 43,
     marginRight: 15,
     marginTop: 10
   },
@@ -65,47 +67,47 @@ export default {
     marginTop: 7
   },
   confirmation: {
-    flexDirection: 'row'
+    flexDirection: "row"
   },
   confirmText: {
-    color: '#ff0a55',
+    color: "#ff0a55",
     fontSize: 12,
     marginRight: 15,
     marginTop: 7
   },
   confirmNo: {
-    color: '#00FF9D',
+    color: "#00FF9D",
     fontSize: 12,
     marginRight: 10,
     marginTop: 7
   },
   indicator: {
     flex: 1,
-    justifyContent: 'center'
+    justifyContent: "center"
   },
   moreContainer: {
-    width: '100%',
-    justifyContent: 'center',
-    alignItems: 'center'
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center"
   },
   more: {
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     borderWidth: 2,
-    borderColor: 'black',
-    alignItems: 'center',
-    justifyContent: 'center',
+    borderColor: "black",
+    alignItems: "center",
+    justifyContent: "center",
     borderRadius: 5,
     height: 28,
     width: 243
   },
   moreText: {
     fontSize: 14,
-    fontFamily: 'OpenSans-Regular',
-    color: 'black'
+    fontFamily: "OpenSans-Regular",
+    color: "black"
   },
   icon: {
     fontSize: 30,
     marginTop: 9,
-    color: '#00FF9D'
+    color: "#00FF9D"
   }
 };
