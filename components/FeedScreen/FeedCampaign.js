9ec0df8fca31c041b5e599c98812fcc3560161b4
Jamie Goodnight <goodnightlambda@gmail.com>
2019-09-26 12:19:02 -0400

    Converted comments_length in FeedCampaign to comments.length

--- components/FeedScreen/FeedCampaign.js
+++ components/FeedScreen/FeedCampaign.js
@@ -1,17 +1,18 @@
-import React, { useState } from "react";
-import { View, Text, Image, TouchableOpacity } from "react-native";
-import moment from "moment";
-
-import { ListItem } from "react-native-elements";
-import { useDispatch } from "react-redux";
-import { AmpEvent } from "../withAmplitude";
+import React, { useState } from 'react';
+import { View, Text, Image, TouchableOpacity, FlatList } from 'react-native';
+import moment from 'moment';
+import { Avatar } from 'react-native-elements';
+import { ListItem } from 'react-native-elements';
+import { useDispatch } from 'react-redux';
+import { AmpEvent } from '../withAmplitude';
 import {
   getProfileData,
   getCampaign,
   toggleCampaignText
-} from "../../store/actions";
+} from '../../store/actions';
 
-import styles from "../../constants/FeedScreen/FeedCampaign";
+import styles from '../../constants/FeedScreen/FeedCampaign';
+import styles2 from '../../constants/Comments/Comments';
 
 const FeedCampaign = props => {
   const dispatch = useDispatch();
@@ -21,7 +22,7 @@ const FeedCampaign = props => {
       return string;
     } else {
       let end = cutoff;
-      const avoidChars = [" ", ",", ".", "!"];
+      const avoidChars = [' ', ',', '.', '!'];
       while (avoidChars.includes(string.charAt(end)) && end >= cutoff - 10) {
         end--;
       }
@@ -33,48 +34,48 @@ const FeedCampaign = props => {
   const currentTime = moment();
   const postTime = moment(createdAt);
   let timeDiff;
-  if (currentTime.diff(postTime, "days") < 1) {
-    if (currentTime.diff(postTime, "hours") < 1) {
-      if (currentTime.diff(postTime, "minutes") < 1) {
-        timeDiff = "just now";
+  if (currentTime.diff(postTime, 'days') < 1) {
+    if (currentTime.diff(postTime, 'hours') < 1) {
+      if (currentTime.diff(postTime, 'minutes') < 1) {
+        timeDiff = 'just now';
       } else {
-        if (currentTime.diff(postTime, "minutes") === 1) {
-          timeDiff = `${currentTime.diff(postTime, "minutes")} MINUTE AGO`;
+        if (currentTime.diff(postTime, 'minutes') === 1) {
+          timeDiff = `${currentTime.diff(postTime, 'minutes')} MINUTE AGO`;
         } else {
-          timeDiff = `${currentTime.diff(postTime, "minutes")} MINUTES AGO`;
+          timeDiff = `${currentTime.diff(postTime, 'minutes')} MINUTES AGO`;
         }
       }
     } else {
-      if (currentTime.diff(postTime, "hours") === 1) {
-        timeDiff = `${currentTime.diff(postTime, "hours")} HOUR AGO`;
+      if (currentTime.diff(postTime, 'hours') === 1) {
+        timeDiff = `${currentTime.diff(postTime, 'hours')} HOUR AGO`;
       } else {
-        timeDiff = `${currentTime.diff(postTime, "hours")} HOURS AGO`;
+        timeDiff = `${currentTime.diff(postTime, 'hours')} HOURS AGO`;
       }
     }
   } else {
-    if (currentTime.diff(postTime, "days") === 1) {
-      timeDiff = `${currentTime.diff(postTime, "days")} DAY AGO`;
+    if (currentTime.diff(postTime, 'days') === 1) {
+      timeDiff = `${currentTime.diff(postTime, 'days')} DAY AGO`;
     } else {
-      timeDiff = `${currentTime.diff(postTime, "days")} DAYS AGO`;
+      timeDiff = `${currentTime.diff(postTime, 'days')} DAYS AGO`;
     }
   }
 
   const goToProfile = async () => {
     await dispatch(getProfileData(data.users_id));
-    AmpEvent("Select Profile from Campaign", {
+    AmpEvent('Select Profile from Campaign', {
       profile: data.username,
       campaign: data.camp_name
     });
-    props.navigation.navigate("Pro");
+    props.navigation.navigate('Pro');
   };
 
   const goToCampaign = async () => {
     await dispatch(getCampaign(data.camp_id));
-    AmpEvent("Select Profile from Campaign", {
+    AmpEvent('Select Profile from Campaign', {
       campaign: data.camp_name,
       profile: data.username
     });
-    props.navigation.navigate("Camp");
+    props.navigation.navigate('Camp');
   };
 
   const toggleText = () => {
@@ -105,7 +106,7 @@ const FeedCampaign = props => {
         style={styles.goToCampaignButton}
         onPress={goToCampaign}
       >
-        <Text style={styles.goToCampaignText}>See Post {">"}</Text>
+        <Text style={styles.goToCampaignText}>See Post {'>'}</Text>
       </TouchableOpacity>
       <View style={styles.campDesc}>
         <Text style={styles.campDescName}>{data.camp_name}</Text>
@@ -121,15 +122,44 @@ const FeedCampaign = props => {
           </Text>
         )}
       </View>
+      {/* <View>
+        <FlatList
+          data={data.comments.slice(0, 2)}
+          // keyExtractor={comment => comment.comment_id}
+          renderItem={({ item }) => {
+            return (
+              <View style={styles2.commentWrapper}>
+                <View style={styles2.commentView}>
+                  <View style={styles2.avatar}>
+                    <Avatar
+                      rounded
+                      source={{
+                        uri: item.profile_image
+                      }}
+                    />
+                  </View>
+                  <View>
+                    <Text style={styles2.username}>{item.username}</Text>
+                    <Text style={styles2.commentBody}>{item.comment_body}</Text>
+                  </View>
+                </View>
+                <View style={styles2.interaction}>
+                  <Text style={styles2.timeText}>{timeDiff}</Text>
+                </View>
+              </View>
+            );
+          }}
+        />
+      </View> */}
       <View>
-        {data.comments_length >= 1 ? (
-          data.comments_length === 1 ? (
+        {data.comments.length >= 1 ? (
+          data.comments.length === 1 ? (
             <Text style={styles.comments} onPress={goToCampaign}>
-              View {data.comments_length} comment
+              View {data.comments.length} comment
             </Text>
           ) : (
             <Text style={styles.comments} onPress={goToCampaign}>
-              View all {data.comments_length} comments
+              View all {data.comments.length} comments
             </Text>
           )
         ) : null}
