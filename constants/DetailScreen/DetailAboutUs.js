f5f3b1d6391aac53c92fe3f0d22a9842bdd641eb
Katie Rose <katiefelten@gmail.com>
2019-10-07 15:03:44 -0400

    :zap: updated profile background and border radius for cards

--- constants/DetailScreen/DetailAboutUs.js
+++ constants/DetailScreen/DetailAboutUs.js
@@ -1,8 +1,8 @@
 export default {
   container: {
-    alignItems: 'flex-start',
+    alignItems: "flex-start",
     margin: 15,
-    textAlign: 'justify',
+    textAlign: "justify",
     lineHeight: 30
   },
   title: {
@@ -10,33 +10,35 @@ export default {
   },
   body: {
     marginTop: 10,
-    flexDirection: 'column',
-    flexWrap: 'nowrap'
+    flexDirection: "column",
+    flexWrap: "nowrap", 
+    fontSize: 15
   },
   iconWrap: {
-    flexWrap: 'wrap',
-    alignItems: 'center',
-    flexDirection: 'row',
-    borderBottomColor: '#eee',
+    flexWrap: "wrap",
+    alignItems: "center",
+    flexDirection: "row",
+    borderBottomColor: "#eee",
     borderBottomWidth: 1,
     marginBottom: 2,
     paddingBottom: 20
   },
   sections: {
-    marginTop: 20,
-    backgroundColor: '#fff',
-    width: '100%',
-    padding: 25
+    marginTop: 10,
+    backgroundColor: "#fff",
+    width: "100%",
+    padding: 25,
+    borderRadius: 5
   },
   title: {
     fontSize: 18,
-    alignSelf: 'center',
+    alignSelf: "center",
     marginLeft: 10,
-    fontFamily: 'OpenSans-Regular'
+    fontFamily: "OpenSans-Regular"
   },
   donateButton: {
-    alignItems: 'center',
-    width: '100%'
+    alignItems: "center",
+    width: "100%"
   },
   forcedMargin: {
     marginTop: 10
