23a8efc403c320908334310fdea200cbb02d0a58
Katie Rose <katiefelten@gmail.com>
2019-10-15 12:07:44 -0400

    :zap: update color updated

--- constants/FeedScreen/FeedUpdate.js
+++ constants/FeedScreen/FeedUpdate.js
@@ -1,33 +1,33 @@
-import { Dimensions } from 'react-native';
+import { Dimensions } from "react-native";
 
-const deviceWidth = Dimensions.get('window').width;
+const deviceWidth = Dimensions.get("window").width;
 
 export default {
   container: {
-    justifyContent: 'center',
+    justifyContent: "center",
     paddingBottom: 2
   },
   orgTitleView: {
-    fontFamily: 'OpenSans-Regular',
+    fontFamily: "OpenSans-Regular",
     fontSize: 17
   },
   updateBar: {
-    backgroundColor: 'rgba(202, 255, 0, 0.5)',
+    backgroundColor: "rgba(202,255,0, 0.7)",
     height: 37,
-    width: '100%',
-    position: 'absolute',
+    width: "100%",
+    position: "absolute",
     zIndex: 1,
     top: 0,
     right: 0,
     bottom: 0,
     left: 0,
-    justifyContent: 'center',
-    alignItems: 'center'
+    justifyContent: "center",
+    alignItems: "center"
   },
   updateBarText: {
-    fontFamily: 'OpenSans-SemiBold',
+    fontFamily: "OpenSans-SemiBold",
     fontSize: 18,
-    color: 'black'
+    color: "black"
   },
   campImgContain: {
     /* Must have a Width && Height or it won't display anything! */
@@ -36,22 +36,22 @@ export default {
     width: deviceWidth
   },
   goToCampaignButton: {
-    backgroundColor: 'rgba(0, 255, 157, 0.5)',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "rgba(0, 255, 157, 0.5)",
+    alignItems: "center",
+    justifyContent: "center",
     height: 37,
-    position: 'absolute',
+    position: "absolute",
     bottom: 0,
-    width: '100%'
+    width: "100%"
   },
   goToCampaignText: {
-    fontFamily: 'OpenSans-SemiBold',
+    fontFamily: "OpenSans-SemiBold",
     fontSize: 18
   },
   likesContainer: {
-    flexDirection: 'row',
+    flexDirection: "row",
     marginTop: 15,
-    alignItems: 'center'
+    alignItems: "center"
   },
   hearts: {
     marginHorizontal: 15
@@ -62,7 +62,7 @@ export default {
   },
   heartFill: {
     fontSize: 30,
-    color: '#e60024'
+    color: "#e60024"
   },
   campDesc: {
     marginLeft: 15,
@@ -70,20 +70,20 @@ export default {
     marginRight: 15
   },
   campDescName: {
-    fontFamily: 'OpenSans-SemiBold',
+    fontFamily: "OpenSans-SemiBold",
     fontSize: 16,
     paddingBottom: 10
   },
   campDescText: {
-    fontFamily: 'OpenSans-Regular',
+    fontFamily: "OpenSans-Regular",
     fontSize: 14,
     lineHeight: 19
   },
   readMore: {
-    color: '#929292'
+    color: "#929292"
   },
   timeText: {
-    color: '#929292',
+    color: "#929292",
     fontSize: 10,
     marginLeft: 15,
     marginTop: 15,
@@ -92,6 +92,6 @@ export default {
   demarcation: {
     marginTop: 15,
     borderTopWidth: 2,
-    borderTopColor: '#eee'
+    borderTopColor: "#eee"
   }
 };
