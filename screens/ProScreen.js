0c049c85957c9373ee3b6898d8b5f92d7595a25e
Jamie Goodnight <goodnightlambda@gmail.com>
2019-10-17 16:43:47 -0400

    :bug: Added Viewport.Tracker to ProScreen

--- screens/ProScreen.js
+++ screens/ProScreen.js
@@ -1,25 +1,26 @@
-import React from "react";
-import { View, ScrollView } from "react-native";
-import { connect } from "react-redux";
-import { getProfileData } from "../store/actions";
-import FeedCampaign from "../components/FeedScreen/FeedCampaign";
-import FeedUpdate from "../components/FeedScreen/FeedUpdate";
-import ProfileHeader from "../components/Profile/ProfileHeader";
-import BackButton from "../components/BackButton";
+import React from 'react';
+import { View, ScrollView } from 'react-native';
+import { connect } from 'react-redux';
+import { Viewport } from '@skele/components';
+import { getProfileData } from '../store/actions';
+import FeedCampaign from '../components/FeedScreen/FeedCampaign';
+import FeedUpdate from '../components/FeedScreen/FeedUpdate';
+import ProfileHeader from '../components/Profile/ProfileHeader';
+import BackButton from '../components/BackButton';
 
 class ProScreen extends React.Component {
   static navigationOptions = ({ navigation }) => {
     return {
-      title: "Profile",
+      title: 'Profile',
       headerStyle: {
-        backgroundColor: "#323338"
+        backgroundColor: '#323338'
       },
-      headerTintColor: "#fff",
+      headerTintColor: '#fff',
       headerTitleStyle: {
-        textAlign: "center",
+        textAlign: 'center',
         flexGrow: 1,
-        alignSelf: "center",
-        fontFamily: "OpenSans-SemiBold"
+        alignSelf: 'center',
+        fontFamily: 'OpenSans-SemiBold'
       },
       headerLeft: <BackButton navigation={navigation} />,
       headerRight: <View />
@@ -30,34 +31,36 @@ class ProScreen extends React.Component {
     const { navigation } = this.props;
     return (
       // creates sticky header
-      <ScrollView stickyHeaderIndices={[0]}>
-        <ProfileHeader
-          navigation={navigation}
-          profile={this.props.selectedProfile}
-          myProfile={false}
-        />
-        {this.props.selectedProfile.campaigns.map(camp => {
-          if (camp.update_id) {
-            return (
-              <FeedUpdate
-                key={`update${camp.update_id}`}
-                data={camp}
-                toggled
-                navigation={navigation}
-              />
-            );
-          } else {
-            return (
-              <FeedCampaign
-                key={camp.camp_id}
-                data={camp}
-                toggled
-                navigation={navigation}
-              />
-            );
-          }
-        })}
-      </ScrollView>
+      <Viewport.Tracker>
+        <ScrollView stickyHeaderIndices={[0]}>
+          <ProfileHeader
+            navigation={navigation}
+            profile={this.props.selectedProfile}
+            myProfile={false}
+          />
+          {this.props.selectedProfile.campaigns.map(camp => {
+            if (camp.update_id) {
+              return (
+                <FeedUpdate
+                  key={`update${camp.update_id}`}
+                  data={camp}
+                  toggled
+                  navigation={navigation}
+                />
+              );
+            } else {
+              return (
+                <FeedCampaign
+                  key={camp.camp_id}
+                  data={camp}
+                  toggled
+                  navigation={navigation}
+                />
+              );
+            }
+          })}
+        </ScrollView>
+      </Viewport.Tracker>
     );
   }
 }
