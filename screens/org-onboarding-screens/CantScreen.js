679bd66df38385fc9e8447f8893760412ff0c101
irisjitomo <irisjitomo@eyahoo.com>
2019-11-12 16:36:31 -0500

    made cantscreen, canscreen, makeaccountscreen

---
+++ screens/org-onboarding-screens/CantScreen.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+const CantScreen = (props) => {
+	return (
+		<View>
+			<View>
+				<Text>What we cannot do...</Text>
+			</View>
+			<View>
+				<View>
+					<Text>
+						Commodo enim cupidatat consequat cupidatat ea Lorem cupidatat consequat aliquip aliqua ullamco
+						id. Consequat sit elit ea labore fugiat culpa eiusmod proident duis. Elit mollit veniam
+						cupidatat minim est incididunt ullamco et laboris minim velit in.
+					</Text>
+				</View>
+				<View>
+					<Text>
+						Commodo enim cupidatat consequat cupidatat ea Lorem cupidatat consequat aliquip aliqua ullamco
+						id. Consequat sit elit ea labore fugiat culpa eiusmod proident duis. Elit mollit veniam
+						cupidatat minim est incididunt ullamco et laboris minim velit in.
+					</Text>
+				</View>
+				<View>
+					<Text>
+						Commodo enim cupidatat consequat cupidatat ea Lorem cupidatat consequat aliquip aliqua ullamco
+						id. Consequat sit elit ea labore fugiat culpa eiusmod proident duis. Elit mollit veniam
+						cupidatat minim est incididunt ullamco et laboris minim velit in.
+					</Text>
+				</View>
+			</View>
+			<View>
+				<TouchableOpacity>
+					<Text>Next</Text>
+				</TouchableOpacity>
+			</View>
+		</View>
+	);
+};
+
+export default CantScreen
