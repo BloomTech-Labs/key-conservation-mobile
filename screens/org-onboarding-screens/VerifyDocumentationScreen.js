8a3aafaeff14022f65a3d7a4d5f85abb59193632
bodhibrdr <julie.gumerman@gmail.com>
2019-11-12 13:44:10 -0700

    verifydocumentationscreen finished

--- screens/org-onboarding-screens/VerifyDocumentationScreen.js
+++ screens/org-onboarding-screens/VerifyDocumentationScreen.js
@@ -4,7 +4,12 @@ import { Button, Text, View } from "react-native";
 const VerifyDocumentationScreen = () => {
     return (
         <View>
-            
+            <Text>Verify your organization </Text>
+            <Text>To prevent fraud, we need to properly vet organization credentials.</Text>
+            <Text>Proof of organization</Text>
+            <Text>Please provide a clear photo of your 501c3 or other state-approved documentation.</Text>
+            <Button title="Photo of documentation" />
+            <Button>Next</Button>
         </View>
     );
 }
