50c04376c56c4c1f7e236e15bc4bffbabde8e3bd
Cameron Alvarado <cameronalvarado@Camerons-Macintosh.local>
2019-11-19 15:48:29 -0800

    most of form built out. .create and .update both work

--- screens/org-onboarding-screens/VerifyOrganizationScreen.js
+++ screens/org-onboarding-screens/VerifyOrganizationScreen.js
@@ -2,36 +2,83 @@ import React, { useState } from "react";
 import { StyleSheet, Switch, Text, TextInput, View, TouchableOpacity} from "react-native";
 import styles from '../../constants/screens/org-onboarding-styles/VerifyOrg.js';
 
-
 const VerifyOrganizationScreen = (props) => {
 
+    const [airtableState, onChangeText] = useState({
+        other_countries: '',
+        multiple_projects: '',
+        affiliations_partnerships: '',
+        conservation_optimism: '',
+        smartphone_access: '',
+        smartphone_type: ''
+    });
 
+    var Airtable = require('airtable');
+    var base = new Airtable({apiKey: 'keybUdphipr0RgMaa'}).base('appbPeeXUSNCQWwnQ');
 
-    const [consOptimism, setConsOptimism] = useState(false);
-    const [smartPhone, setSmartPhone] = useState(false);
+    const airtableID = props.navigation.getParam('airtableID', 'defaultValue');
+
+    const updateAirtable = () => {
+        base('Table 1').update([
+            {
+              "id": airtableID,
+              "fields": {
+                "other_countries": airtableState.other_countries,
+                "affiliations_partnerships": airtableState.affiliations_partnerships,
+                "conservation_optimism": airtableState.conservation_optimism,
+                "multiple_projects": airtableState.multiple_projects,
+                "smartphone_access": airtableState.smartphone_access,
+                // "smartphone_type": airtableState.smartphone_type
+              }
+            }
+          ], function(err, records) {
+            if (err) {
+              console.error(err);
+              return;
+            }
+            records.forEach(function(record) {
+              console.log(record.getId());
+            });
+          });
+    }
     
     return (
         <View style={styles.obBody}>
             <Text style={styles.obTitle}>Tell us more about your organization</Text>
+
             <Text style={styles.obFieldName}>In what countries does your organization work?</Text>
-            <TextInput style={styles.obTextInput} />
+            <TextInput 
+            style={styles.obTextInput}
+            onChangeText={text => onChangeText({ ...airtableState, other_countries: text })}
+            value={airtableState.other_countries} />
+
             <Text style={styles.obFieldName}>Projects your organization is working on:</Text>
-            <TextInput style={styles.obTextInput}/>
+            <TextInput 
+            style={styles.obTextInput}
+            onChangeText={text => onChangeText({ ...airtableState, multiple_projects: text })}
+            value={airtableState.multiple_projects} />
+
             <Text style={styles.obFieldName}>Current partnerships and affiliations:</Text>
-            <TextInput style={styles.obTextInput}/>
+            <TextInput 
+            style={styles.obTextInput}
+            onChangeText={text => onChangeText({ ...airtableState, affiliations_partnerships: text })}
+            value={airtableState.affiliations_partnerships} />
+
             <Text>Will you join us in Conservation Optimism?</Text>
             <Switch 
-                value={consOptimism}
-                onValueChange={newValue => setConsOptimism(newValue)}
+                value={airtableState.conservation_optimism}
+                onValueChange={newValue => onChangeText({ ...airtableState, conservation_optimism: newValue})}
             />
+
             <Text>Does your organization have access to a smartphone?</Text>
             <Switch 
-                value={smartPhone}
-                onValueChange={newValue => setSmartPhone(newValue)}
+                value={airtableState.smartphone_access}
+                onValueChange={newValue => onChangeText({...airtableState, smartphone_access: newValue })}
             />
 
             <TouchableOpacity style={styles.obFwdContainer}
                 onPress={() => {
+                    updateAirtable();
                     props.navigation.navigate("VerifyDocumentation")
                 }}
             >
