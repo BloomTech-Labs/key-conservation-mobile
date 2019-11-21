ec4c9419adb7bb4ab7dd13c6eb83f8d82f992751
Yilun Weng <wengellen@gmail.com>
2019-11-20 18:17:16 -0800

    :bug: Organization onboarding screen
    Add back missing file, resolve merge conflict, hookup steps"

--- screens/org-onboarding-screens/VerifyDocumentationScreen.js
+++ screens/org-onboarding-screens/VerifyDocumentationScreen.js
@@ -1,49 +1,59 @@
-import React, {Component} from "react";
-import { Button, Text, View, TouchableOpacity } from "react-native";
+import React, { Component } from 'react';
+import { Button, Text, View, TouchableOpacity } from 'react-native';
 import styles from '../../constants/screens/org-onboarding-styles/VerifyDocs.js';
-
 
 import * as WebBrowser from 'expo-web-browser';
 
 export default class VerifyDocumentationScreen extends Component {
-    state = {
-		result: null
-    };
-    
-    _handlePressButtonAsync = async () => {
-        try {
-        let result = await WebBrowser.openAuthSessionAsync('https://airtable.com/shrkK93NtoOkfnMP8');
-        let redirectData;
-        if (result.url) {
-            redirectData = "https://airtable.com/shrkK93NtoOkfnMP8"
-        }
-        this.setState({ result, redirectData });
-    } catch (error) {
-        alert(error);
-    }
-    };
+  state = {
+    result: null
+  };
 
-    render() {
-        const { navigation } = this.props;
+  _handlePressButtonAsync = async () => {
+    try {
+      let result = await WebBrowser.openAuthSessionAsync(
+        'https://airtable.com/shrkK93NtoOkfnMP8'
+      );
+      let redirectData;
+      if (result.url) {
+        redirectData = 'https://airtable.com/shrkK93NtoOkfnMP8';
+      }
+      this.setState({ result, redirectData });
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  render() {
+    const { navigation } = this.props;
 
     return (
-        <View style={styles.obBody}>
-            <Text style={styles.obTitle}>Verify your organization </Text>
-            <Text style={styles.obText}>To prevent fraud, we need to properly vet organization credentials.</Text>
-            <Text style={styles.obSubtitle}>Proof of organization</Text>
-            <Text style={styles.obTextBottom}>Please provide a clear photo of your 501(c)(3) or other state-approved documentation.</Text>
-            <TouchableOpacity  style={styles.obUploadBtn} onPress={this._handlePressButtonAsync}>
-                <Text style={styles.obText}>Photo of Documentation</Text>
-            </TouchableOpacity>
+      <View style={styles.obBody}>
+        <Text style={styles.obTitle}>Verify your organization </Text>
+        <Text style={styles.obText}>
+          To prevent fraud, we need to properly vet organization credentials.
+        </Text>
+        <Text style={styles.obSubtitle}>Proof of organization</Text>
+        <Text style={styles.obTextBottom}>
+          Please provide a clear photo of your 501(c)(3) or other state-approved
+          documentation.
+        </Text>
+        <TouchableOpacity
+          style={styles.obUploadBtn}
+          onPress={() => this._handlePressButtonAsync()}
+        >
+          <Text style={styles.obText}>Photo of Documentation</Text>
+        </TouchableOpacity>
         <View style={styles.spacer} />
-            <TouchableOpacity style={styles.obFwdContainer}
-                onPress={() => {
-                    navigation.navigate("AlmostDone")
-                }}            
-            >
-                <Text style={styles.obFwdBtnText}>Next</Text>
-            </TouchableOpacity>
-        </View>
+        <TouchableOpacity
+          style={styles.obFwdContainer}
+          onPress={() => {
+            navigation.navigate('ReviewYourInfo');
+          }}
+        >
+          <Text style={styles.obFwdBtnText}>Next</Text>
+        </TouchableOpacity>
+      </View>
     );
-}
+  }
 }
