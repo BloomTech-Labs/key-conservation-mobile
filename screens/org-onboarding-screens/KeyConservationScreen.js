a7a2961b92380de502e1773454c3bb457afb6c7e
Katie Rose <katiefelten@gmail.com>
2019-11-21 15:29:43 -0500

    :zap: updated svg in lets get started page

--- screens/org-onboarding-screens/KeyConservationScreen.js
+++ screens/org-onboarding-screens/KeyConservationScreen.js
@@ -1,32 +1,26 @@
-import React from "react";
-import { Button, View, Text, TouchableOpacity } from "react-native";
+import React from 'react';
+import { Button, View, Text, TouchableOpacity } from 'react-native';
 import styles from '../../constants/screens/org-onboarding-styles/KeyConservation.js';
 
-import SvgUri from "react-native-svg-uri";
+import SvgUri from 'react-native-svg-uri';
 
-const KeyConservationScreen = (props) => {
-    return (
-        <View style={styles.obBody}>
-            <Text style={styles.obTitle}>
-                Let's go over how Key Conservation works
-            </Text>
-            <TouchableOpacity style={styles.obFwdContainer}
-                onPress={() => {
-                    props.navigation.navigate("Can")
-                }}
-            >
-                <Text style={styles.obFwdBtnText}>Next</Text>
-            </TouchableOpacity>
-
-            {/* <SvgUri
-              fill='#3b3b3b'
-              width='25'
-              height='25'
-              source={require("./../../assets/icons/How-it-works.svg")}
-            /> */}
-
-        </View>
-    );
-}
+const KeyConservationScreen = props => {
+  return (
+    <View style={styles.obBody}>
+      <SvgUri
+        style={styles.svg}
+        source={require('./../../assets/icons/How-it-works.svg')}
+      />
+      <TouchableOpacity
+        style={styles.obFwdContainer}
+        onPress={() => {
+          props.navigation.navigate('Can');
+        }}
+      >
+        <Text style={styles.obFwdBtnText}>Next</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
 
 export default KeyConservationScreen;
