2b87f7c907ec6597a723a10c561af8c5565734f9
irisjitomo <irisjitomo@eyahoo.com>
2019-11-21 17:46:22 -0500

    added svg to make account screen

--- screens/org-onboarding-screens/MakeAccountScreen.js
+++ screens/org-onboarding-screens/MakeAccountScreen.js
@@ -3,26 +3,31 @@ import React, { useEffect, useState } from 'react';
 import { View, ScrollView, Text, TextInput, TouchableOpacity, KeyboardAvoidingView, TouchableOpacityBase } from 'react-native';
 import styles from '../../constants/screens/org-onboarding-styles/MakeAccount.js';
 
+import SvgUri from 'react-native-svg-uri';
+
 const MakeAccountScreen = (props) => {
 
-	const [screen, setScreen] = useState(false)
+	// const [screen, setScreen] = useState(false)
 	
-    useEffect(() => {
-		console.log('hello')
-	}, [])
+    // useEffect(() => {
+	// 	console.log('hello')
+	// }, [])
 	
-    useEffect(() => {
-        const timer = setTimeout(() => {setScreen(true)}, 3000)
-        if (screen === true) {
-            props.navigation.navigate("TellAboutOrganization");
-            return() => clearTimeout(timer);
-        }
-        setScreen(false)
-	})
+    // useEffect(() => {
+    //     const timer = setTimeout(() => {setScreen(true)}, 3000)
+    //     if (screen === true) {
+    //         props.navigation.navigate("TellAboutOrganization");
+    //         return() => clearTimeout(timer);
+    //     }
+    //     setScreen(false)
+	// })
 
 	return (
 		<View style={styles.obBody}>
-			<Text style={styles.obTitle}>Let's get acquainted</Text>
+			<SvgUri 
+				style={styles.svg}
+				source={require('../../assets/icons/Timed.svg')}
+			/>
 				<TouchableOpacity 
 					style={styles.obFwdContainer}
                 	onPress={() => {
