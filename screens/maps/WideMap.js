c73eb60cd0a7e9355c2e338314f95ee910ff0943
bodhibrdr <julie.gumerman@gmail.com>
2019-12-03 15:23:32 -0700

    airtable get request functional

--- screens/maps/WideMap.js
+++ screens/maps/WideMap.js
@@ -3,6 +3,36 @@ import MapView from 'react-native-maps';
 import { StyleSheet, View, Dimensions } from 'react-native';
 
 export default class WideMap extends React.Component {
+
+  componentDidMount() {
+    var Airtable = require('airtable');
+      var base = new Airtable({apiKey: 'keybUdphipr0RgMaa'}).base('appbPeeXUSNCQWwnQ');
+
+      base('Table 1').select({
+          // Selecting the first 3 records in Grid view:
+          maxRecords: 100,
+          view: "Grid view"
+      }).eachPage(function page(records, fetchNextPage) {
+          // This function (`page`) will get called for each page of records.
+          console.log("START HERE!", records.map(record => {
+            return record._rawJson.fields
+          }))
+          // records.forEach(function(record) {
+              
+          //     console.log('Retrieved', record.get('org_name'), record.get('address'), record.get('country'));
+          // });
+
+          // To fetch the next page of records, call `fetchNextPage`.
+          // If there are more records, `page` will get called again.
+          // If there are no more records, `done` will get called.
+          fetchNextPage();
+
+      }, function done(err) {
+          if (err) { console.error(err); return; }
+      });
+        }
+
+
   render() {
     return (
       <View style={styles.container}>
