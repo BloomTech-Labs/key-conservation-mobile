4e471400d53034a1103443beda9f58686af69c73
System Administrator <jon.s.solari@gmail.com>
2019-12-03 13:04:31 -0500

    latest attempt

---
+++ screens/maps/WideMap.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import { StyleSheet, View } from "react-native";
+import MapboxGL from "@react-native-mapbox-gl/maps";
+
+MapboxGL.setAccessToken("pk.eyJ1Ijoia2V5LWNvbnNlcnZhdGlvbiIsImEiOiJjazNxNDM1b3kwOXpvM2NxZG1hbXgxM2NkIn0.2Jn1IWJADh381J-567JItg");
+
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
+  },
+  container: {
+    height: 300,
+    width: 300,
+    backgroundColor: "tomato"
+  },
+  map: {
+    flex: 1
+  }
+});
+
+export default class WideMap extends Component {
+  componentDidMount() {
+    MapboxGL.setTelemetryEnabled(false);
+  }
+
+  render() {
+    return (
+      <View style={styles.page}>
+        <View style={styles.container}>
+          <MapboxGL.MapView style={styles.map} />
+        </View>
+      </View>
+    );
+  }
+}
