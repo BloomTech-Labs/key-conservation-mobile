015d6e93e458cd26f4b129d52da83d89f3dd48ed
Blake Goms <blakegoms@gmail.com>
2019-12-10 08:51:25 -0800

    added redux

--- screens/maps/WideMap.js
+++ screens/maps/WideMap.js
@@ -1,43 +1,44 @@
 import React, { useEffect, useState } from 'react';
+import {connect} from 'react-redux'
+import {getOrganizations} from '../../store/actions'
 import MapView, { Marker } from 'react-native-maps';
 import { StyleSheet, View, Dimensions } from 'react-native';
-import Axios from "axios";
 // import Geocode from 'react-geocode'
 import LocationIQ from 'react-native-locationiq';
-import * as SecureStore from "expo-secure-store";
-
+​
 const WideMap = (props) => {
-    
-    const [coordinates, setCoordinates] = useState({latitude: '', longitude: ''});
-    useEffect(async () => {
-        const token = await SecureStore.getItemAsync("accessToken", {});
-        Axios.get("https://key-conservation.herokuapp.com/api/maps",
-            {  headers:
-                    {  Accept: "application/json",
-                        Authorization: `Bearer ${token}`,
-                        "Content-Type": "application/json"
-                    } })
-        .then(async function(response) {
-            console.log('response',response)
-            
-        })
-        .catch(function (error) {
-            console.log(error)
-        })
-        
-        
+    const [coordinates, setCoordinates] = useState();
+    useEffect( () => {
+        props.getOrganizations();
     }, []);
-    LocationIQ.init("pk.b1c961f18c509bdb2a91cb0a3c0d78ca");
-    LocationIQ.search("Eiffel Tower, Paris, France")
-    .then(json => {
-        var lat = json[0].lat;
-        var lon = json[0].lon;
-        console.log("Coordinates:", lat, lon);
-        setCoordinates({latitude: parseFloat(lat), longitude: parseFloat(lon)})
-    })
-    .catch(error => console.warn(error));
     
-    console.log("COORDINATES!!!!!", coordinates)
+    useEffect( () => {
+        props.locations.map(location => {
+            convertLocationToCoords(location)
+        })
+        return;
+    }, [props.locations]);
+    
+    //
+    const convertLocationToCoords = (location)=>{
+        console.log('convertLocationToCoords - ', location)
+        LocationIQ.init("pk.b1c961f18c509bdb2a91cb0a3c0d78ca");
+        LocationIQ.search(location)
+       
+        // LocationIQ.search("Eiffel Tower, Paris, France")
+        .then(json => {
+            // return json.
+            console.log('json', json)
+            var lat = json[0].lat;
+            var lon = json[0].lon;
+            // console.log("Coordinates:", lat, lon);
+            setCoordinates({latitude: parseFloat(lat), longitude: parseFloat(lon)})
+        })
+        .catch(error => console.warn(error));
+    }
+    //
+    
+    // console.log("COORDINATES!!!!!", coordinates)
     return (
         <View style={styles.container}>
             <MapView style={styles.mapStyle}>
@@ -49,7 +50,7 @@ const WideMap = (props) => {
         </View>
     );
 }
-
+​
 const styles = StyleSheet.create({
     container: {
         // flex: 1,
@@ -62,5 +63,20 @@ const styles = StyleSheet.create({
         height: Dimensions.get('window').height,
     },
 });
-
-export default WideMap;
+​
+const mapPropsToState = (state)=>{
+    const locations = state.organizations.map(org => {
+        return org.location
+    }).filter(location => location!==null)
+    
+    console.log('organizations', locations)
+    return {
+        organizations: state.organizations,
+        locations: locations
+    }
+}
+​
+export default connect(
+  mapPropsToState,
+ {getOrganizations})
+ (WideMap);
