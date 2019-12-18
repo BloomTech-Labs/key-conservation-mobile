aeb51a16745a02aeabf8c885514032c1ca252209
Blake Goms <blakegoms@gmail.com>
2019-12-18 09:23:07 -0800

    updated accountsettings font

--- constants/screens/AccountSettingsScreen.js
+++ constants/screens/AccountSettingsScreen.js
@@ -1,22 +1,22 @@
 export default {
-    scrollBG: {
+  scrollBG: {
     backgroundColor: "#E5E5E5",
-    height: '100%'
+    height: "100%"
   },
-    touchableView: {
-      backgroundColor: 'black',
-      alignItems: 'center',
-      justifyContent: 'center',
-      borderRadius: 5,
-      height: 35
-    },
-    touchableText: {
-      color: '#fff',
-      textTransform: 'uppercase',
-      fontWeight: 'bold',
-      letterSpacing: 2
-    },
-   sections: {
+  touchableView: {
+    backgroundColor: "black",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 5,
+    height: 35
+  },
+  touchableText: {
+    color: "#fff",
+    textTransform: "uppercase",
+    fontWeight: "bold",
+    letterSpacing: 2
+  },
+  sections: {
     marginTop: 8,
     backgroundColor: "#fff",
     width: "100%",
@@ -30,9 +30,9 @@ export default {
     fontSize: 18,
     alignSelf: "center",
     marginLeft: 10,
-    fontFamily: "Futura-Bold"
+    fontFamily: "Futura-Medium"
   },
-    iconWrap: {
+  iconWrap: {
     flexWrap: "wrap",
     alignItems: "center",
     flexDirection: "row",
@@ -45,40 +45,39 @@ export default {
     flexWrap: "wrap",
     alignItems: "center",
     flexDirection: "row",
-    marginLeft: '7%'
+    marginLeft: "7%"
   },
   linkText: {
     fontSize: 18,
     alignSelf: "center",
-    marginLeft: '4%',
+    marginLeft: "4%",
     fontFamily: "Futura",
-    marginTop: '4%'
+    marginTop: "4%"
   },
   logoutButton: {
-    marginTop: '5%',
+    marginTop: "5%",
     width: 40,
     height: 40
   },
-    buttonContainer: {
-      width: '30%',
-      height: 30,
-      marginTop: 10,
-      flexDirection: "column",
-      flexWrap: "nowrap",
-      marginLeft: '35%',
-      marginRight: '35%',
-      borderRadius: 4,
-      backgroundColor: '#00FF9D',
-      justifyContent: 'center'
-    },
-    buttonText: {
-      fontFamily: 'Futura-Bold',
-      letterSpacing: .8,
-      textAlign: 'center',
-      color: 'black',
-      justifyContent: 'center',
-      alignItems: 'center',
-      fontSize: 16
-    },
-  };
-  
+  buttonContainer: {
+    width: "30%",
+    height: 30,
+    marginTop: 10,
+    flexDirection: "column",
+    flexWrap: "nowrap",
+    marginLeft: "35%",
+    marginRight: "35%",
+    borderRadius: 4,
+    backgroundColor: "#00FF9D",
+    justifyContent: "center"
+  },
+  buttonText: {
+    fontFamily: "Futura-Bold",
+    letterSpacing: 0.8,
+    textAlign: "center",
+    color: "black",
+    justifyContent: "center",
+    alignItems: "center",
+    fontSize: 16
+  }
+};
