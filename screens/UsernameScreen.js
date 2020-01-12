0f3cee7c903647e458d4a78c09e644be11f69a4f
Katie Rose <katiefelten@gmail.com>
2020-01-12 12:10:07 -0500

    :zap: updated username screen styling

--- screens/UsernameScreen.js
+++ screens/UsernameScreen.js
@@ -1,31 +1,31 @@
-import React from "react";
-import { Text, View, TextInput, Button, TouchableOpacity } from "react-native";
-import { ScrollView } from "react-navigation";
-import { connect } from "react-redux";
-import { AmpEvent } from "../components/withAmplitude";
-import * as SecureStore from "expo-secure-store";
-import styles from "../constants/screens/UsernameScreen";
-import { postUser, logout } from "../store/actions";
+import React from 'react';
+import { Text, View, TextInput, Button, TouchableOpacity } from 'react-native';
+import { ScrollView } from 'react-navigation';
+import { connect } from 'react-redux';
+import { AmpEvent } from '../components/withAmplitude';
+import * as SecureStore from 'expo-secure-store';
+import styles from '../constants/screens/UsernameScreen';
+import { postUser, logout } from '../store/actions';
 
-import Constants from "expo-constants";
-import * as WebBrowser from "expo-web-browser";
+import Constants from 'expo-constants';
+import * as WebBrowser from 'expo-web-browser';
 
-var Airtable = require("airtable");
-var base = new Airtable({ apiKey: "keybUdphipr0RgMaa" }).base(
-  "appbPeeXUSNCQWwnQ"
+var Airtable = require('airtable');
+var base = new Airtable({ apiKey: 'keybUdphipr0RgMaa' }).base(
+  'appbPeeXUSNCQWwnQ'
 );
 
 class UsernameScreen extends React.Component {
   state = {
-    usernameInput: "",
-    error: "",
+    usernameInput: '',
+    error: '',
     result: null,
-    id: ""
+    id: ''
   };
 
   updateAirtable = () => {
-    console.log("UsernameScreen updateAirtable triggered");
-    base("Table 1").update(
+    console.log('UsernameScreen updateAirtable triggered');
+    base('Table 1').update(
       [
         {
           id: this.state.id,
@@ -49,10 +49,10 @@ class UsernameScreen extends React.Component {
 
   handlePress = async () => {
     const { error } = this.props;
-    const sub = await SecureStore.getItemAsync("sub", {});
-    const email = await SecureStore.getItemAsync("email", {});
-    const role = await SecureStore.getItemAsync("roles", {});
-    const id = await SecureStore.getItemAsync("airtableID", {});
+    const sub = await SecureStore.getItemAsync('sub', {});
+    const email = await SecureStore.getItemAsync('email', {});
+    const role = await SecureStore.getItemAsync('roles', {});
+    const id = await SecureStore.getItemAsync('airtableID', {});
     const username = this.state.usernameInput;
 
     if (username.length > 4) {
@@ -68,30 +68,30 @@ class UsernameScreen extends React.Component {
         email: email
       };
       await this.props.postUser(user);
-      AmpEvent("Account Created");
+      AmpEvent('Account Created');
       this.props.navigation.navigate(
-        this.props.error ? "CreateAccount" : "Loading"
+        this.props.error ? 'CreateAccount' : 'Loading'
       );
-      await SecureStore.deleteItemAsync("airtableID", {});
-      await SecureStore.deleteItemAsync("vettingEmail", {});
-      await SecureStore.deleteItemAsync("isVetting", {});
+      await SecureStore.deleteItemAsync('airtableID', {});
+      await SecureStore.deleteItemAsync('vettingEmail', {});
+      await SecureStore.deleteItemAsync('isVetting', {});
       // Deletes vetting variables, checked by 'LoadingScreen', to allow additional organizations to sign up.
     } else {
       this.setState({
-        error: "Username is required to be at least 5 characters"
+        error: 'Username is required to be at least 5 characters'
       });
     }
   };
 
   logoutPress = async () => {
-    await SecureStore.deleteItemAsync("sub", {});
-    await SecureStore.deleteItemAsync("email", {});
-    await SecureStore.deleteItemAsync("roles", {});
-    await SecureStore.deleteItemAsync("id", {});
-    await SecureStore.deleteItemAsync("accessToken", {});
+    await SecureStore.deleteItemAsync('sub', {});
+    await SecureStore.deleteItemAsync('email', {});
+    await SecureStore.deleteItemAsync('roles', {});
+    await SecureStore.deleteItemAsync('id', {});
+    await SecureStore.deleteItemAsync('accessToken', {});
     this.props.logout();
 
-    const logoutURL = "https://key-conservation.auth0.com/v2/logout?federated";
+    const logoutURL = 'https://key-conservation.auth0.com/v2/logout?federated';
 
     if (Constants.platform.ios) {
       await WebBrowser.openAuthSessionAsync(logoutURL).then(result => {
@@ -102,7 +102,7 @@ class UsernameScreen extends React.Component {
         this.setState({ result });
       });
     }
-    this.props.navigation.navigate("Logout");
+    this.props.navigation.navigate('Logout');
   };
 
   render() {
@@ -111,12 +111,16 @@ class UsernameScreen extends React.Component {
         <View style={styles.sectionContainer}>
           <View style={styles.Card} />
           <View style={styles.textContainer}>
-            <Text style={{ textAlign: "center" }}>Thanks for signing up!</Text>
-            <Text style={{ textAlign: "center" }}>Choose a username!</Text>
+            <Text style={styles.obTitle}>
+              You're in! Next step: please choose a username.
+            </Text>
+            <Text style={styles.obText}>
+              Type in a username that clearly describes your organization.
+            </Text>
           </View>
           <TextInput
-            returnKeyType="go"
-            placeholder="ex: Enter Username"
+            returnKeyType='go'
+            placeholder='BirdRescueTN'
             style={styles.inputContain}
             onChangeText={text => this.setState({ usernameInput: text })}
             value={this.state.usernameInput}
@@ -125,16 +129,16 @@ class UsernameScreen extends React.Component {
         </View>
         <View style={{ height: 20, margin: 25 }}>
           {this.state.error ? (
-            <Text style={{ textAlign: "center", color: "red" }}>
+            <Text style={{ textAlign: 'center', color: 'red' }}>
               {this.state.error}
             </Text>
           ) : this.props.error.config &&
-            this.props.error.config.method === "get" ? (
-            <Text style={{ textAlign: "center", color: "green" }}>
+            this.props.error.config.method === 'get' ? (
+            <Text style={{ textAlign: 'center', color: 'green' }}>
               Please choose a username and enter it above
             </Text>
           ) : this.props.error.message ? (
-            <Text style={{ textAlign: "center", color: "red" }}>
+            <Text style={{ textAlign: 'center', color: 'red' }}>
               Failed to create user. Please try another username
             </Text>
           ) : null}
@@ -144,15 +148,15 @@ class UsernameScreen extends React.Component {
           style={styles.touchableButton}
         >
           <View style={styles.touchableView}>
-            <Text style={styles.touchableText}>Continue</Text>
+            <Text style={styles.touchableText}>CONTINUE</Text>
           </View>
         </TouchableOpacity>
         <TouchableOpacity
           onPress={this.logoutPress}
           style={styles.touchableButton}
         >
-          <View style={styles.touchableView}>
-            <Text style={styles.touchableText}>Logout</Text>
+          <View style={styles.obFwdContainer}>
+            <Text style={styles.obFwdBtnText}>LOGOUT</Text>
           </View>
         </TouchableOpacity>
       </ScrollView>
@@ -161,7 +165,7 @@ class UsernameScreen extends React.Component {
 }
 
 UsernameScreen.navigationOptions = {
-  title: "Sign Up"
+  title: 'Sign Up'
 };
 
 const mapStateToProps = state => ({
