b601b48a460e38c6278e67f8e7b61e636e349308
Katie Rose <katiefelten@gmail.com>
2020-01-21 12:16:33 -0500

    :zap: fixed typo in loadingscreen

--- screens/LogoutScreen.js
+++ screens/LogoutScreen.js
@@ -16,45 +16,15 @@ const LogoutScreen = props => {
     >
       <Text style={styles.text}>Logout successful!</Text>
       <TouchableOpacity
-        style={stylie.touchableContainer}
+        style={style.touchableContainer}
         onPress={() => {
           props.navigation.navigate({ routeName: 'Loading' });
         }}
       >
-        <Text style={stylie.touchableText}>Log Back In</Text>
+        <Text style={style.touchableText}>Log Back In</Text>
       </TouchableOpacity>
     </ImageBackground>
   );
 };
 
 export default LogoutScreen;
-
-// const stylie = StyleSheet.create({
-//   buttonContainer: {
-//     width: '70%',
-//     height: 50,
-//     marginBottom: 18,
-//     borderRadius: 5,
-//     fontFamily: 'Lato',
-//     backgroundColor: '#00FF9D',
-//     shadowColor: 'rgba(0, 0, 0, 0.25)',
-//     shadowOffset: {
-//       width: 0,
-//       height: 4
-//     },
-//     shadowRadius: 4,
-//     shadowOpacity: 1,
-//     justifyContent: 'center',
-//     marginTop: 10
-//   },
-//   buttonText: {
-//     fontFamily: 'Lato-Bold',
-//     fontStyle: 'normal',
-//     letterSpacing: 0,
-//     textAlign: 'center',
-//     color: 'black',
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     fontSize: 20
-//   }
-// });
