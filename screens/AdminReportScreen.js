01c5c24477fcfbf6acf69d8883bca915342353d3
zacharybaca <baca.zach@icloud.com>
2020-01-28 15:16:31 -0600

    Untrack unnecessary expo configurations

--- screens/AdminReportScreen.js
+++ screens/AdminReportScreen.js
@@ -1,12 +1,43 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import { getReports } from '../store/actions';
+import { connect } from 'react-redux';
 
-const AdminReportScreen = () => {
+const AdminReportScreen = props => {
   return (
-    <View>
-      <Text>We Made It!</Text>
+    <View style={styles.container}>
+      <View style={styles.section}>
+        <Text>Current Reports</Text>
+
+        {reports.map(report => {
+          <View style={styles.reportCard} key={report.id}>
+            {r}
+          </View>;
+        })}
+      </View>
+      <View style={styles.section}></View>
     </View>
   );
 };
 
-export default AdminReportScreen;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'rgb(242,242,251)'
+  },
+  section: {
+    flex: 1,
+    backgroundColor: 'white',
+    borderRadius: 5,
+    margin: 4
+  },
+  reportCard: {
+    flexDirection: row,
+    height: 48
+  }
+});
+const mapStateToProps = state => ({
+  reports: state.reports
+});
+
+export default connect(mapStateToProps, { getReports })(AdminReportScreen);
