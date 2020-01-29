d35e395c02051166fcaef71f4ca66168aebb8560
Arielle Hollingsworth <arizephyr123@gmail.com>
2020-01-29 12:14:39 -0800

    built out arrow in ReportCard
    Co-authored by: mtourjoman01@hotmail.com

---
+++ components/Reports/ReportCard.js
@@ -0,0 +1,104 @@
+import React from "react";
+import { View, Text, Image, StyleSheet } from "react-native";
+import SvgUri from "react-native-svg-uri";
+
+const ReportCard = props => {
+  let type;
+  switch (props.table_name) {
+    case "campaignUpdates":
+      type = "Campaign Reported";
+      break;
+    case "campaigns":
+      type = "Campaign Reported";
+      break;
+    case "comments":
+      type = "Comment Reported";
+      break;
+    case "users":
+      type = "User Reported";
+      break;
+
+    default:
+      type = props.table_name;
+  }
+
+  return (
+    <View style={styles.reportCard}>
+      <View style={styles.left}>
+        <View style={styles.imageContainer}>
+          <Image source={{ uri: props.image }} style={styles.image}></Image>
+        </View>
+        <View style={styles.reportInfo}>
+          <Text style={styles.reportName}>{props.name}</Text>
+          <Text style={styles.reportType}>{type.toUpperCase()}</Text>
+        </View>
+      </View>
+      <View style={styles.right}>
+        <View style={styles.reportCount}></View>
+        <View style={styles.arrowContainer}>
+          <SvgUri
+            style={styles.arrowIcon}
+            source={require("../../assets/icons/chevron-left-solid.svg")}
+            fill={"#000000"}
+            width="25"
+            height="100%"
+          />
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  reportCard: {
+    flex: 1,
+    flexDirection: "row",
+    height: 48,
+    alignItems: "center",
+    justifyContent: "space-between"
+  },
+  imageContainer: {
+    height: 48,
+    width: 48
+  },
+  image: {
+    flex: 1,
+    borderRadius: 40,
+    height: null,
+    width: null
+  },
+  reportInfo: {
+    marginHorizontal: 6
+  },
+  reportName: {
+    fontSize: 16
+  },
+  reportType: {
+    fontSize: 12,
+    color: "grey"
+  },
+  reportCount: {},
+  arrowContainer: {
+    width: "10%",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100%"
+  },
+  arrowIcon: {
+    transform: [{ rotateZ: "180deg" }],
+    paddingVertical: 18,
+  },
+  left: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  right: {
+    justifyContent: "flex-end",
+    flexDirection: "row",
+    flex: 1,
+    alignItems: "center",
+  }
+});
+
+export default ReportCard;
--- screens/AdminReportScreen.js
+++ screens/AdminReportScreen.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
 import { getReports } from "../store/actions";
 import { connect } from "react-redux";
-import arrowIcon from "../assets/icons/chevron-left-solid.svg";
+import ReportCard from "../components/Reports/ReportCard";
 
 const AdminReportScreen = props => {
   const init = () => {
@@ -16,44 +16,7 @@ const AdminReportScreen = props => {
         <Text>Current Reports</Text>
 
         {props.reports.data?.reports.map(report => {
-          let type;
-          console.log(report.table_name);
-          switch (report.table_name) {
-            case "campaignUpdates":
-              type = "Campaign Reported";
-              break;
-            case "campaigns":
-              type = "Campaign Reported";
-              break;
-            case "comments":
-              type = "Comment Reported";
-              break;
-            case "users":
-              type = "User Reported";
-              break;
-
-            default:
-              type = report.table_name;
-          }
-
-          return (
-            <View style={styles.reportCard} key={report.id}>
-              <View style={styles.imageContainer}>
-                <Image
-                  source={{ uri: report.image }}
-                  style={styles.image}
-                ></Image>
-              </View>
-              <View style={styles.reportInfo}>
-                <Text style={styles.reportName}>{report.name}</Text>
-                <Text style={styles.reportType}>{type.toUpperCase()}</Text>
-              </View>
-              <View style={styles.reportCount}></View>
-              <View style={styles.arrowContainer}>
-                <Image style={styles.arrowIcon} source={arrowIcon}></Image>
-              </View>
-            </View>
-          );
+          return <ReportCard {...report} key={report.id}/>
         })}
       </View>
       <View style={styles.section}></View>
@@ -64,7 +27,7 @@ const AdminReportScreen = props => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "rgb(242,242,251)"
+    backgroundColor: "rgb(242,242,251)",
   },
   section: {
     flex: 1,
@@ -72,44 +35,6 @@ const styles = StyleSheet.create({
     borderRadius: 8,
     margin: 8,
     padding: 8
-  },
-  reportCard: {
-    flex: 1,
-    flexDirection: "row",
-    height: 48,
-    alignItems: "center"
-  },
-  imageContainer: {
-    height: 48,
-    width: 48
-  },
-  image: {
-    flex: 1,
-    borderRadius: 40,
-    height: null,
-    width: null
-  },
-  reportInfo: {
-    marginHorizontal: 6
-  },
-  reportName: {
-    fontSize: 16
-  },
-  reportType: {
-    fontSize: 12,
-    color: "grey"
-  },
-  reportCount: {},
-  arrowContainer: {
-    width: "10%",
-    borderWidth: 1,
-    height: "100%"
-  },
-  arrowIcon: {
-    width: null,
-    height: null,
-    color: "black"
-
   }
 });
 const mapStateToProps = state => ({
