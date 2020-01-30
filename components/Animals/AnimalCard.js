346877796f8f6d88c5473ae0ca3a402afe85e787
Arielle Hollingsworth <arizephyr123@gmail.com>
2020-01-29 23:47:01 -0800

    animal details rendering from Animals to AnimalCard

--- components/Animals/AnimalCard.js
+++ components/Animals/AnimalCard.js
@@ -1,13 +1,71 @@
 import React, { useState } from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, Image, StyleSheet } from "react-native";
+import SvgUri from "react-native-svg-uri";
 
 const AnimalCard = props => {
-
+console.log(props);
     return (
-        <View>
-
+       <View style={styles.reportCard}>
+           <View style={styles.imageContainer}>
+               <Text>{props.animal.name}</Text>
+          <Image source={props.animal.image} style={styles.image}></Image>
         </View>
+
+       </View>
     )
-}
+};
+
+const styles = StyleSheet.create({
+    reportCard: {
+      flex: 1,
+      flexDirection: "row",
+      height: 48,
+      marginVertical: 4,
+      alignItems: "center",
+      justifyContent: "space-between"
+    },
+    imageContainer: {
+      height: 48,
+      width: 48
+    },
+    image: {
+      flex: 1,
+      borderRadius: 40,
+      height: null,
+      width: null
+    },
+    reportInfo: {
+      marginHorizontal: 6
+    },
+    reportName: {
+      fontSize: 16
+    },
+    reportType: {
+      fontSize: 12,
+      color: "grey"
+    },
+    reportCount: {},
+    arrowContainer: {
+      width: "10%",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100%"
+    },
+    arrowIcon: {
+      transform: [{ rotateZ: "180deg" }],
+      paddingVertical: 18,
+    },
+    left: {
+      flex: 1,
+      flexDirection: "row",
+      alignItems: "center",
+    },
+    right: {
+      justifyContent: "flex-end",
+      flexDirection: "row",
+      flex: 1,
+      alignItems: "center",
+    }
+  });
 
 export default AnimalCard;
--- components/Animals/Animals.js
+++ components/Animals/Animals.js
@@ -1,13 +1,92 @@
 import React, { useState } from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  ScrollView
+} from "react-native";
+
+import AnimalCard from "./AnimalCard";
 
 const Animals = props => {
+  const animalData = [
+    {
+      image: "../../assets/images/africanelephant.png",
+      name: "African Elephant",
+      wwfLink: "https://www.worldwildlife.org/species/african-elephant"
+    },
+    {
+      image: "../../assets/images/whaleshark.png",
+      name: "Whale Shark",
+      wwfLink: "https://www.worldwildlife.org/species/whale-shark"
+    },
+    {
+      image: "../../assets/images/africanelephant.png",
+      name: "Giant Tortoise",
+      wwfLink: "https://www.worldwildlife.org/species/giant-tortoise"
+    }
+  ];
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>
+        Click to learn more about these beautiful Animals!
+      </Text>
+      <View style={styles.reportList}>
+        <ScrollView>
+          {animalData.map(animal => (
+            <AnimalCard animal={animal} />
+          ))}
+        </ScrollView>
+      </View>
+    </View>
+  );
+};
 
-    return (
-        <View>
-<Text>Animals Page!</Text>
-        </View>
-    )
-}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "rgb(242,242,251)"
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    padding: 16,
+    marginHorizontal: 8
+  },
+  section: {
+    flex: 1,
+    backgroundColor: "white",
+    borderRadius: 8,
+    margin: 8
+  },
+  tabSelector: {
+    width: "100%",
+    flexDirection: "row",
+    height: 32,
+    alignItems: "center",
+    justifyContent: "space-between"
+  },
+  tab: {
+    flex: 1,
+    height: "100%",
+    alignItems: "center"
+  },
+  tabText: {
+    flex: 1,
+    letterSpacing: 0.7,
+    fontSize: 17
+  },
+  reportList: {
+    flex: 1,
+    padding: 12,
+    flexDirection: "column"
+  },
+  selectedTab: {
+    width: "100%",
+    backgroundColor: "#00FF9D",
+    height: 3
+  }
+});
 
 export default Animals;
