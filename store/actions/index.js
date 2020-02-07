9048bb5ea8c3782fdfbb6ea66d680252460da3ca
Mohammad Tourjoman <mtourjoman01@hotmail.com>
2020-02-06 22:35:48 -0800

    Fix seturl to automatically set based on environment

--- store/actions/index.js
+++ store/actions/index.js
@@ -35,11 +35,12 @@ const axiosWithAuth = (dispatch, req) => {
 };
 
 // url for heroku staging vs production server
+// Automatically set based on environment
 // production
-// const seturl = 'https://key-conservation.herokuapp.com/api/';
+const PRODUCTION = 'https://key-conservation.herokuapp.com/api/';
 // staging
-// const seturl = 'https://key-conservation-staging.herokuapp.com/api/';
-const seturl = 'http://192.168.1.13:8000/api/';
+const STAGING = 'https://key-conservation-staging.herokuapp.com/api/';
+const seturl = __DEV__ ? STAGING : PRODUCTION;
 
 const filterUrls = (keys, object) => {
   // If a user doesn't include http or https in their URL this function will add it.
