5d15127aa31739673669e12279b5c9dba000641c
Arielle Hollingsworth <arizephyr123@gmail.com>
2020-02-13 12:45:32 -0800

    done to back arrow, moved to left, created BackArrowHeader.js

--- constants/screens/LoadingScreen.js
+++ constants/screens/LoadingScreen.js
@@ -1,41 +1,46 @@
-export default {
-	container          : {
-		flex            : 1,
-		width           : '100%',
-		backgroundColor : 'white',
-		alignItems      : 'center',
-		justifyContent  : 'center',
-		height          : '100%',
-	},
-	logo               : {
-		width  : 189,
-		height : 189,
-	},
-	text               : {
-		marginTop     : 30,
-		fontSize      : 30,
-		color         : 'white',
-		fontFamily    : 'Lato-Bold',
-		paddingBottom : 40,
-	},
-	indicator          : {
-		marginTop : 50,
-	},
-	touchableContainer : {
-		width           : 262,
-		height          : 55,
-		borderRadius    : 20,
-		backgroundColor : '#00FF9D',
-		alignItems      : 'center',
-		justifyContent  : 'center',
-		flexDirection   : 'column',
-	},
-	touchableText      : {
-		fontFamily    : 'Lato-Bold',
-		letterSpacing : 2,
-		fontStyle     : 'normal',
-		fontSize      : 22,
-		lineHeight    : 25,
-		color         : '#000000',
-	},
-};
+import { StyleSheet, Dimensions } from 'react-native';
+import { responsiveFontSize } from 'react-native-responsive-dimensions';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: Dimensions.width,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: Dimensions.height
+  },
+  logo: {
+    width: 189,
+    height: 189
+  },
+  text: {
+    marginTop: 30,
+    fontSize: 30,
+    color: 'white',
+    fontFamily: 'Lato-Bold',
+    paddingBottom: 40
+  },
+  indicator: {
+    marginTop: 50
+  },
+  touchableContainer: {
+    width: 262,
+    height: 55,
+    borderRadius: 20,
+    backgroundColor: '#d7ff43',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column'
+  },
+  touchableText: {
+    fontFamily: 'Lato-Bold',
+    letterSpacing: 2,
+    fontStyle: 'normal',
+    fontSize: 22,
+    lineHeight: 25,
+    color: '#000000'
+  }
+});
+
+export default styles;
--- screens/AccountSettingsScreen.js
+++ screens/AccountSettingsScreen.js
@@ -1,41 +1,37 @@
-import React from "react";
-import { Text, Image, View, TouchableOpacity } from "react-native";
-import SvgUri from "react-native-svg-uri";
-import { ScrollView } from "react-navigation";
-import { connect } from "react-redux";
-import { logout } from "../store/actions";
-import * as SecureStorage from "expo-secure-store";
-import DoneButton from "../components/DoneButton";
+import React from 'react';
+import { Text, Image, View, TouchableOpacity } from 'react-native';
+import { ScrollView } from 'react-navigation';
+import { connect } from 'react-redux';
+import { logout } from '../store/actions';
+import * as SecureStorage from 'expo-secure-store';
+//import * as WebBrowser from 'expo-web-browser';
+//import Constants from 'expo-constants';
 
-import styles from "../constants/screens/AccountSettingsScreen";
-import * as WebBrowser from "expo-web-browser";
-
-import Constants from "expo-constants";
-
-import Smile from "../assets/jsicons/bottomnavigation/Smile";
+import styles from '../constants/screens/AccountSettingsScreen';
+import Smile from '../assets/jsicons/bottomnavigation/Smile';
+import BackArrowHeader from '../assets/jsicons/miscIcons/BackArrowHeader';
 
 class AccountSettingsScreen extends React.Component {
   state = {
     result: null,
-    roles: ""
+    roles: ''
   };
 
   static navigationOptions = ({ navigation }) => {
     return {
-      title: "Account Settings",
+      title: 'ACCOUNT SETTINGS',
       headerStyle: {
-        backgroundColor: "#323338"
+        backgroundColor: '#323338'
       },
-      headerTintColor: "#fff",
-      headerRight: () => (
-        <DoneButton
-          pressAction={navigation.getParam("done")}
-        />
+      headerTintColor: '#fff',
+      headerLeft: () => (
+        <BackArrowHeader pressAction={navigation.getParam('done')} />
       )
     };
   };
+
   getRole = async () => {
-    const myRoles = await SecureStorage.getItemAsync("roles", {});
+    const myRoles = await SecureStorage.getItemAsync('roles', {});
     this.setState({ roles: myRoles });
   };
 
@@ -45,22 +41,22 @@ class AccountSettingsScreen extends React.Component {
   }
 
   done = () => {
-    if (this.state.roles === "conservationist") {
-      this.props.navigation.navigate("MyPro");
+    if (this.state.roles === 'conservationist') {
+      this.props.navigation.navigate('MyPro');
     } else {
-      this.props.navigation.navigate("MySupPro");
+      this.props.navigation.navigate('MySupPro');
     }
   };
   viewReports = () => {
-    this.props.navigation.navigate("AdminScreen");
+    this.props.navigation.navigate('AdminScreen');
   };
   logoutPress = async () => {
-    await SecureStorage.deleteItemAsync("sub", {});
-    await SecureStorage.deleteItemAsync("email", {});
-    await SecureStorage.deleteItemAsync("roles", {});
-    await SecureStorage.deleteItemAsync("id", {});
-    await SecureStorage.deleteItemAsync("userId", {});
-    await SecureStorage.deleteItemAsync("accessToken", {});
+    await SecureStorage.deleteItemAsync('sub', {});
+    await SecureStorage.deleteItemAsync('email', {});
+    await SecureStorage.deleteItemAsync('roles', {});
+    await SecureStorage.deleteItemAsync('id', {});
+    await SecureStorage.deleteItemAsync('userId', {});
+    await SecureStorage.deleteItemAsync('accessToken', {});
     // await SecureStorage.deleteItemAsync("airtableKey", {}); // for development
     this.props.logout();
 
@@ -86,12 +82,6 @@ class AccountSettingsScreen extends React.Component {
             <View style={styles.sections}>
               <View style={styles.iconWrap}>
                 <Smile />
-                {/* <SvgUri
-                  fill="#3b3b3b"
-                  width="25"
-                  height="25"
-                  source={require("../assets/icons/user.svg")}
-                /> */}
                 <Text style={styles.title}>Admin Controls</Text>
               </View>
               <TouchableOpacity
@@ -99,7 +89,7 @@ class AccountSettingsScreen extends React.Component {
                 onPress={this.viewReports}
               >
                 <Image
-                  source={require("../assets/icons/logout.png")}
+                  source={require('../assets/icons/logout.png')}
                   style={styles.logoutButton}
                 />
                 <Text style={styles.linkText}>Manage Reports</Text>
@@ -109,24 +99,17 @@ class AccountSettingsScreen extends React.Component {
           <View style={styles.sections}>
             <View style={styles.iconWrap}>
               <Smile />
-              {/* <SvgUri
-                fill="#3b3b3b"
-                width="25"
-                height="25"
-                source={require("../assets/icons/user.svg")}
-              /> */}
-
-              <Text style={styles.title}>Logout Of Your Profile</Text>
+              <Text style={styles.title}>Log Out Of Your Profile</Text>
             </View>
             <TouchableOpacity
               style={styles.linkWrap}
               onPress={this.logoutPress}
             >
               <Image
-                source={require("../assets/icons/logout.png")}
+                source={require('../assets/icons/logout.png')}
                 style={styles.logoutButton}
               />
-              <Text style={styles.linkText}>Logout</Text>
+              <Text style={styles.linkText}>Log Out</Text>
             </TouchableOpacity>
           </View>
         </View>
