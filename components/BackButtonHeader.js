556dfb0404259555f41781964a9ce2d75e5dbd91
Arielle Hollingsworth <arizephyr123@gmail.com>
2020-02-18 11:22:54 -0800

    styling updates in components/AccountSettingScreen

--- components/BackButtonHeader.js
+++ components/BackButtonHeader.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import { TouchableOpacity, View, Text } from 'react-native';
 import BackArrowHeader from '../assets/jsicons/miscIcons/BackArrowHeader';
+import styles from '../constants/screens/AccountSettingsScreen';
 
 const BackButtonHeader = props => {
   return (
     <TouchableOpacity onPress={props.pressAction} style={{ padding: 18 }}>
-      <View
-        style={{
-          alignItems: 'center',
-          justifyContent: 'center',
-          borderRadius: 5,
-          height: 35
-        }}
-      >
+      <View style={styles.backArrowTouch}>
         <BackArrowHeader />
       </View>
     </TouchableOpacity>
