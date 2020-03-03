d1ac0ed79f842d2778fada59125ff10a1c824bbc
Katie Rose <32432575+katie-rose@users.noreply.github.com>
2020-03-03 17:48:23 -0500

    updated seturl

--- store/actions/index.js
+++ store/actions/index.js
@@ -52,7 +52,7 @@ const axiosWithAuth = (dispatch, req) => {
 // url for heroku staging vs production server
 // Automatically set based on environment
 // production
-const PRODUCTION = 'https://key-conservation.herokuapp.com/api/';
+const seturl = PRODUCTION = 'https://key-conservation.herokuapp.com/api/';
 // staging
 // const STAGING = 'https://key-conservation-staging.herokuapp.com/api/';
 // const seturl = STAGING = 'https://key-conservation-staging.herokuapp.com/api/';
