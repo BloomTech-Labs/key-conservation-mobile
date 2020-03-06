c0c19a2aa7e0f2431552566dba96ebc400bea156
Mohammad Tourjoman <mtourjoman01@hotmail.com>
2020-03-06 07:54:45 -0800

    Fix seturl stuff

--- store/actions/index.js
+++ store/actions/index.js
@@ -52,11 +52,10 @@ const axiosWithAuth = (dispatch, req) => {
 // url for heroku staging vs production server
 // Automatically set based on environment
 // production
-const seturl = 'http://192.168.1.13:8000/api/';
-// const seturl = PRODUCTION = 'https://key-conservation.herokuapp.com/api/';
+const PRODUCTION = 'https://key-conservation.herokuapp.com/api/';
 // staging
-// const STAGING = 'https://key-conservation-staging.herokuapp.com/api/';
-// const seturl = STAGING = 'https://key-conservation-staging.herokuapp.com/api/';
+const STAGING = 'https://key-conservation-staging.herokuapp.com/api/';
+const seturl = __DEV__ ? STAGING : PRODUCTION;
 
 const filterUrls = (keys, object) => {
   // If a user doesn't include http or https in their URL this function will add it.
