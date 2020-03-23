fbf38bd996499003eafeb38c52b3fd38a59e57d0
Katie Rose Felten <katiefelten@gmail.com>
2020-03-23 13:28:32 -0400

    :zap: switched heroku to staging server

--- store/actions/index.js
+++ store/actions/index.js
@@ -52,10 +52,9 @@ const axiosWithAuth = (dispatch, req) => {
 // url for heroku staging vs production server
 // comment out either server depending on testing needs
 // production
-const seturl = 'https://key-conservation.herokuapp.com/api/';
+// const seturl = 'https://key-conservation.herokuapp.com/api/';
 // staging
-// const seturl = 'https://key-conservation-staging.herokuapp.com/api/';
-
+const seturl = 'https://key-conservation-staging.herokuapp.com/api/';
 
 const filterUrls = (keys, object) => {
   // If a user doesn't include http or https in their URL this function will add it.
