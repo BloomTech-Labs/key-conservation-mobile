cce9347d4229c64f4355056a3e1332c6146316dc
Katie Rose Felten <katiefelten@gmail.com>
2020-04-08 15:10:48 -0400

    :zap: fixed heres what you can expect

--- screens/org-onboarding-screens/ToExpectScreen.js
+++ screens/org-onboarding-screens/ToExpectScreen.js
@@ -7,59 +7,62 @@ import CheckCircleDuo from '../../assets/jsicons/OnBoarding/CheckCircleDuo';
 import NavigateButton from './formElement/NavigateButton';
 import NavigateBack from './formElement/NavigateBack';
 
-const ToExpectScreen = props => {
+const ToExpectScreen = (props) => {
   return (
     <React.Fragment>
-      <View style={styles.arrowView}>
-        <NavigateBack
-          onButtonPress={() => {
-            props.navigation.navigate('HeyThere');
-          }}
-          paddingHorozontal='15%'
-        />
-      </View>
       <ScrollView>
-        <View style={styles.obBody}>
-          <Text style={styles.obTitle}>Here's what you can expect:</Text>
+        <View style={styles.mainContainer}>
+          <View style={styles.arrowView}>
+            <NavigateBack
+              onButtonPress={() => {
+                props.navigation.navigate('HeyThere');
+              }}
+              paddingHorozontal="15%"
+            />
+          </View>
 
-          <View style={styles.contentWrapper}>
-            <View style={styles.iconWrapper}>
-              <CheckCircleDuo fill='#3b3b3b' width='25' height='25' />
+          <View style={styles.obBody}>
+            <Text style={styles.obTitle}>Here's what you can expect:</Text>
+
+            <View style={styles.contentWrapper}>
+              <View style={styles.iconWrapper}>
+                <CheckCircleDuo fill="#3b3b3b" width="25" height="25" />
+              </View>
+              <View style={{ width: '90%' }}>
+                <Text style={styles.obSubtitle}>Overview</Text>
+                <Text style={styles.obText}>
+                  You already know how Key Conservation can help connect you
+                  with individual contributors. That's why you're here! We'll
+                  keep it brief.
+                </Text>
+              </View>
             </View>
-            <View style={{ width: '90%' }}>
-              <Text style={styles.obSubtitle}>Overview</Text>
-              <Text style={styles.obText}>
-                You already know how Key Conservation can help connect you with
-                individual contributors. That's why you're here! We'll keep it
-                brief.
-              </Text>
+            <View style={styles.contentWrapper}>
+              <View style={styles.iconWrapper}>
+                <CheckCircleDuo fill="#3b3b3b" width="25" height="25" />
+              </View>
+              <View style={{ width: '90%' }}>
+                <Text style={styles.obSubtitle}>Register</Text>
+                <Text style={styles.obText}>
+                  Let's set up your account! You'll need to fill out a form,
+                  upload your credentials, and set up a profile on the following
+                  screens.
+                </Text>
+              </View>
             </View>
-          </View>
-          <View style={styles.contentWrapper}>
-            <View style={styles.iconWrapper}>
-              <CheckCircleDuo fill='#3b3b3b' width='25' height='25' />
-            </View>
-            <View style={{ width: '90%' }}>
-              <Text style={styles.obSubtitle}>Register</Text>
-              <Text style={styles.obText}>
-                Let's set up your account! You'll need to fill out a form,
-                upload your credentials, and set up a profile on the following
-                screens.
-              </Text>
-            </View>
-          </View>
-          <View style={styles.contentWrapper}>
-            <View style={styles.iconWrapper}>
-              <CheckCircleDuo fill='#3b3b3b' width='25' height='25' />
-            </View>
-            <View style={{ width: '90%' }}>
-              <Text style={styles.obSubtitle}>Get Verified</Text>
-              <Text style={[styles.obText, styles.obTextBottom]}>
-                Once your application has been approved, you'll receive a survey
-                by email followed by a welcome kit. Once the survey is complete
-                your organization will be live on the Key App and you can start
-                adding connections and campaigns!
-              </Text>
+            <View style={styles.contentWrapper}>
+              <View style={styles.iconWrapper}>
+                <CheckCircleDuo fill="#3b3b3b" width="25" height="25" />
+              </View>
+              <View style={{ width: '90%' }}>
+                <Text style={styles.obSubtitle}>Get Verified</Text>
+                <Text style={[styles.obText, styles.obTextBottom]}>
+                  Once your application has been approved, you'll receive a
+                  survey by email followed by a welcome kit. Once the survey is
+                  complete your organization will be live on the Key App and you
+                  can start adding connections and campaigns!
+                </Text>
+              </View>
             </View>
           </View>
         </View>
@@ -69,7 +72,7 @@ const ToExpectScreen = props => {
           onButtonPress={() => {
             props.navigation.navigate('KeyConservation');
           }}
-          label='Next'
+          label="Next"
         />
       </View>
     </React.Fragment>
