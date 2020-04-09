cdac2246519ba8f4346d3630ba2dfd7af9a6dee4
Katie Rose Felten <katiefelten@gmail.com>
2020-04-09 11:51:35 -0400

    :zap: style tweaks

--- screens/org-onboarding-screens/HeyThereScreen.js
+++ screens/org-onboarding-screens/HeyThereScreen.js
@@ -7,7 +7,7 @@ import { useDispatch } from 'react-redux';
 import NavigateButton from './formElement/NavigateButton';
 import NavigateBack from './formElement/NavigateBack.js';
 
-const HeyThereScreen = props => {
+const HeyThereScreen = (props) => {
   const dispatch = useDispatch();
 
   return (
@@ -20,7 +20,7 @@ const HeyThereScreen = props => {
           onButtonPress={() => {
             dispatch(logout());
           }}
-          color='#FFF'
+          color="#FFF"
         />
       </View>
       <View style={styles.obBody}>
@@ -40,8 +40,8 @@ const HeyThereScreen = props => {
           onButtonPress={() => {
             props.navigation.navigate('ToExpect');
           }}
-          color='white'
-          label='Next'
+          color="white"
+          label="Next"
         />
       </View>
     </ImageBackground>
