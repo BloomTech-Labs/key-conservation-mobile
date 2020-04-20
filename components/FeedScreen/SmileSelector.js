bf77525240702bedd72e90cafc1c94e979f261e5
moreira.kd@outlook.com <moreira.kd@outlook.com>
2020-04-20 16:11:20 -0400

    horizontal

--- components/FeedScreen/SmileSelector.js
+++ components/FeedScreen/SmileSelector.js
@@ -1,13 +1,21 @@
 import React, { useState } from 'react';
-import { TouchableOpacity, View, StyleSheet, SafeAreaView } from 'react-native';
+import {
+  TouchableOpacity,
+  View,
+  StyleSheet,
+  SafeAreaView,
+  Text,
+} from 'react-native';
 import EmojiSelector, { Categories } from 'react-native-emoji-selector';
 import CheckMark from '../../assets/jsicons/miscIcons/CheckMark';
 
 const SmileSelector = (props) => {
   const [isVisible, setIsVisible] = useState(false);
+  const [emoji, yesEmoji] = useState([]);
+  console.log(emoji, 'emoji!');
 
   return (
-    <View>
+    <SafeAreaView style={styles.container}>
       <TouchableOpacity
         onPress={() => {
           setIsVisible(!isVisible);
@@ -15,16 +23,48 @@ const SmileSelector = (props) => {
       >
         <CheckMark />
       </TouchableOpacity>
-      {isVisible && (
-        <EmojiSelector
-          category={Categories.emotion}
-          onEmojiSelected={(emoji) => console.log(emoji)}
-          placeholder="Search..."
-          showHistory={true}
-        />
-      )}
-    </View>
+
+      <View style={styles.display}>
+        {emoji.map((e, i) => {
+          return (
+            <Text
+              key={i}
+              style={{ fontSize: 64, backgroundColor: 'transparent' }}
+            >
+              {e}
+            </Text>
+          );
+        })}
+      </View>
+
+      <View>
+        {isVisible && (
+          <EmojiSelector
+            category={Categories.emotion}
+            onEmojiSelected={(e) => yesEmoji([...emoji, e])}
+            placeholder="Search..."
+            showHistory={true}
+          />
+        )}
+      </View>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  display: {
+    flexWrap:'wrap', 
+    margin: 24,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+  },
+});
 
 export default SmileSelector;
