dca8d2cac292d93185603812d892b3d4545e1fd4
agohorel <3317203+agohorel@users.noreply.github.com>
2020-04-28 16:51:37 -0400

    group and display count of like-emojis

--- components/FeedScreen/SmileSelector.js
+++ components/FeedScreen/SmileSelector.js
@@ -9,11 +9,31 @@ import {
 } from 'react-native';
 import EmojiSelector, { Categories } from 'react-native-emoji-selector';
 import PlusSignCircle from '../../assets/jsicons/PlusSignCircle';
+import { Badge } from 'react-native-elements';
 
 const SmileSelector = (props) => {
   const [isVisible, setIsVisible] = useState(false);
   const [emoji, setEmoji] = useState([]);
-  console.log(emoji, 'emoji!');
+
+  const setEmojis = (reaction) => {
+    // check if the pressed emoji exists in the array of emojis for this post
+    const existing = emoji.filter((e) => e.emoji === reaction);
+
+    // if the emoji already exists, increment it's count property
+    if (existing.length) {
+      const updated = emoji.map((e) => {
+        if (e.emoji === reaction) {
+          return { ...e, count: e.count + 1 };
+        } else return e;
+      });
+      setEmoji(updated);
+    } else {
+      // otherwise add it to the array and initialize it's count to 1
+      setEmoji([...emoji, { emoji: reaction, count: 1 }]);
+    }
+  };
+
+  console.log(emoji);
 
   return (
     <SafeAreaView style={styles.container}>
@@ -28,14 +48,33 @@ const SmileSelector = (props) => {
 
         <View style={styles.display}>
           {emoji.map((e, i) => {
-            if (i < 8) {
+            if (i < 7) {
               return (
-                <Text
-                  key={i}
-                  style={{ fontSize: 20, backgroundColor: 'transparent' }}
-                >
-                  {e}
-                </Text>
+                <View key={i} style={styles.emojiContainer}>
+                  <Text
+                    style={{ fontSize: 20, backgroundColor: 'transparent' }}
+                  >
+                    {e.emoji}
+                  </Text>
+
+                  {e.count > 1 && (
+                    <Badge
+                      textStyle={{
+                        color: 'black',
+                        fontSize: 14,
+                      }}
+                      badgeStyle={{
+                        backgroundColor: '#CAFF03',
+                      }}
+                      containerStyle={{
+                        position: 'absolute',
+                        top: -8,
+                        right: -8,
+                      }}
+                      value={e.count}
+                    />
+                  )}
+                </View>
               );
             }
           })}
@@ -46,7 +85,7 @@ const SmileSelector = (props) => {
         {isVisible && (
           <EmojiSelector
             category={Categories.emotion}
-            onEmojiSelected={(e) => setEmoji([...emoji, e])}
+            onEmojiSelected={(e) => setEmojis(e)}
             placeholder="Search..."
             showHistory={true}
             columns={8}
@@ -67,13 +106,20 @@ const styles = StyleSheet.create({
   },
   display: {
     flexWrap: 'wrap',
-    margin: 5,
     alignItems: 'center',
     justifyContent: 'center',
     flexDirection: 'row',
   },
   displayedEmojiWrapper: {
     flexDirection: 'row',
+  },
+  emojiContainer: {
+    justifyContent: 'center',
+    borderRadius: 12,
+    backgroundColor: '#F5F5F5',
+    margin: 1,
+    marginTop: 5,
+    padding: 1,
   },
 });
 
