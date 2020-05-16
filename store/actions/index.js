3aa066366c9ccc52acb8aa046b77fb02a1e22f4b
Mohammad Tourjoman <mtourjoman01@hotmail.com>
2020-05-16 13:19:46 -0700

    Fix feed issues

--- store/actions/index.js
+++ store/actions/index.js
@@ -62,8 +62,8 @@ const axiosWithAuth = (dispatch, req) => {
 // production
 // export const seturl = 'https://key-conservation.herokuapp.com/api/';
 // staging
-// export const seturl = 'https://key-conservation-staging.herokuapp.com/api/';
-export const seturl = 'http://192.168.1.146:8000/api/';
+export const seturl = 'https://key-conservation-staging.herokuapp.com/api/';
+// export const seturl = 'http://192.168.1.146:8000/api/';
 
 const filterUrls = (keys, object) => {
   // If a user doesn't include http or https in their URL this function will add it.
@@ -430,7 +430,6 @@ export const getFeed = (startAt = undefined, size = 8) => (dispatch) => {
     return aaxios
       .get(`${seturl}feed?startAt=${startAt}&size=${size}`)
       .then((res) => {
-        console.log(res.data);
         dispatch({
           type: startAt < now ? EXPAND_FEED_SUCCESS : GET_FEED_SUCCESS,
           payload: res.data,
