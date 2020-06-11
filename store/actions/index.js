d4899127de603f01d3946f1b4a341b4e27e1bff1
Mohammad Tourjoman <mtourjoman01@hotmail.com>
2020-06-11 10:55:36 -0700

    Update to correct URL

--- store/actions/index.js
+++ store/actions/index.js
@@ -62,8 +62,8 @@ const axiosWithAuth = (dispatch, req) => {
 // production
 // export const seturl = 'https://key-conservation.herokuapp.com/api/';
 // staging
-// export const seturl = 'https://key-conservation-staging.herokuapp.com/api/';
-export const seturl = 'http://192.168.1.146:8000/api/';
+export const seturl = 'https://key-conservation-staging.herokuapp.com/api/';
+// export const seturl = 'http://192.168.1.146:8000/api/';
 
 const filterUrls = (keys, object) => {
   // If a user doesn't include http or https in their URL this function will add it.
